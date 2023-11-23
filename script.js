4c5a875ec9392a36ef230d37fee9d7845942ca0c
tomcabanau <151138369+tomcabanau@users.noreply.github.com>
2023-11-23 15:18:00 +0100

    interface

---
+++ script.js
@@ -0,0 +1,5 @@
+function navigateToCarPage(carId) {
+    // Remplacez 'car1', 'car2', 'car3' par les identifiants réels de vos voitures
+    // Naviguez vers la page de la voiture concernée
+    window.location.href = 'car-page.html?id=' + carId;
+}

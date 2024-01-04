aed8cbf42bc3537e52e024f34c360f73730e5173
tomcabanau <151138369+tomcabanau@users.noreply.github.com>
2024-01-04 14:47:23 +0100

    1

--- script.js
+++ script.js
@@ -49,4 +49,55 @@ document.addEventListener('DOMContentLoaded', function () {
             clickable: true,
         },
     });
+    // Configuration des particules
+    particlesJS('particles-js', {
+        particles: {
+            number: {
+                value: 100,
+                density: {
+                    enable: true,
+                    value_area: 800
+                }
+            },
+            color: {
+                value: "#ffffff"
+            },
+            size: {
+                value: 3,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 4,
+                    size_min: 0.3,
+                    sync: false
+                }
+            },
+            line_linked: {
+                enable: false
+            },
+            move: {
+                enable: true,
+                speed: 0.5,
+                direction: "top",
+                random: true,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+            }
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: {
+                    enable: false,
+                },
+                onclick: {
+                    enable: false,
+                },
+                resize: true
+            },
+        },
+        retina_detect: true
+    });
+    document.getElementById('particles-js-container').classList.add('bottom-start');
 });

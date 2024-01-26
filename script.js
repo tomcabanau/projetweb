6e6f201583454a1a311ffdee6b7cedc958a445e4
tomcabanau <tom.cabanau@gmail.com>
2024-01-26 10:41:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -44,6 +44,14 @@ function highlightAndNavigate(carWidget) {
     carWidget.scrollIntoView({ behavior: 'smooth' });
 }
 
+// Fonction executée lors de l'utilisation du clic droit.
+$(document).bind("contextmenu",function() {
+    // Si vous voulez ajouter un message d'alerte
+    alert('Merci de respecter le travail du webmaster en ne copiant pas le contenu sans autorisation');
+    // On indique au navigateur de ne pas réagir en cas de clic droit.
+    return false;
+});
+
 // Cette partie du code est exécutée lorsque le document HTML est complètement chargé.
 document.addEventListener('DOMContentLoaded', function () {
     // Initialise un objet Swiper pour créer un effet de galerie de voitures.

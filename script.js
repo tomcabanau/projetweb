090975fa21be68b46639996f1558dbc6c816c2d6
tomcabanau <tom.cabanau@gmail.com>
2024-01-26 10:49:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Fonction executée lors de l'utilisation du clic droit.
 $(document).bind("contextmenu", function () {
-    // Si vous voulez ajouter un message d'alerte
     alert('Merci de respecter le travail du webmaster en ne copiant pas le contenu sans autorisation');
-    // On indique au navigateur de ne pas réagir en cas de clic droit.
     return false;
 });
 
